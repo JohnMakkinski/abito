52b4bc5ddd634e929828ea31bcf96a7181bd5c1e
JohnMakkinski <73485356+JohnMakkinski@users.noreply.github.com>
2025-12-13 08:52:02 +0800

    Сделали адаптивную вёрстку

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 10,
-  slidesPerView: 6,
+  slidesPerView: 4,
   freeMode: true,
   watchSlidesProgress: true,
+  breakpoints: {
+    // Когда ширина экрана >= 768px
+    768: {
+      slidesPerView: 6, // Показываем 6 слайда
+    },
+  },
 });
 var swiper2 = new Swiper(".mySwiper2", {
   spaceBetween: 10,
@@ -13,4 +19,16 @@ var swiper2 = new Swiper(".mySwiper2", {
   thumbs: {
     swiper: swiper,
   },
+});
+
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".navbar-panel");
+const menuCloseBtn = document.querySelector(".close-menu");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("is-open");
+});
+
+menuCloseBtn.addEventListener("click", () => {
+  menu.classList.toggle("is-open");
 });
